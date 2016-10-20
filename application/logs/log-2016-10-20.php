f0af27724be50cf4c6d6bb4192afa5ff4f92cd3d
s-sakhraoui <sanaasakhraoui@gmail.com>
2016-10-20 10:47:10 +0100

    sqtasgh

---
+++ application/logs/log-2016-10-20.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-20 10:45:11 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 10:45:11 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 08:45:11 --> 404 Page Not Found: Uploads/sliders
+ERROR - 2016-10-20 10:45:18 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 10:45:18 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 10:45:25 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:40:36 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:40:55 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:40:55 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:45:39 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:45:39 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
