58de8c86dfaac1a6f6ed3594a82da417a741233d
s-sakhraoui <sanaasakhraoui@gmail.com>
2016-10-20 10:56:56 +0100

    css

--- application/logs/log-2016-10-20.php
+++ application/logs/log-2016-10-20.php
@@ -11,3 +11,15 @@ ERROR - 2016-10-20 11:40:55 --> Could not find the language line "Home slider"
 ERROR - 2016-10-20 11:40:55 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
 ERROR - 2016-10-20 11:45:39 --> Could not find the language line "Home slider"
 ERROR - 2016-10-20 11:45:39 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:47:34 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:47:34 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:47:54 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:47:54 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:48:44 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:48:44 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:51:39 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:51:39 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:52:27 --> Could not find the language line "Home slider"
+ERROR - 2016-10-20 11:52:27 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\admin\utilities\home_slider.php on line 6 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:52:54 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
+ERROR - 2016-10-20 11:52:54 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp\www\CRM\live\application\helpers\perfex_html_helper.php 844
