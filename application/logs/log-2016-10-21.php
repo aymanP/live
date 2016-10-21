e38c765a21ce9a48cdc461e928a98ef0771ca29b
AymanP <elhaouari.ayman@gmail.com>
2016-10-21 17:54:37 +0200

    merging

---
+++ application/logs/log-2016-10-21.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-21 17:50:05 --> Query error: Champ 'Array' inconnu dans field list - Invalid query: INSERT INTO `tblprojects` (`name`, `clientid`, `contactid`, `progress_from_tasks`, `progress`, `billing_type`, `status`, `project_cost`, `project_rate_per_hour`, `start_date`, `deadline`, `description`, `project_created`, `addedfrom`) VALUES ('projet1', '3', Array, 1, '0', '2', '2', 0, '', '2016-09-07', '2016-09-08', '', '2016-10-21', '3')
+ERROR - 2016-10-21 17:50:06 --> Could not find the language line "Home slider"
+ERROR - 2016-10-21 17:50:06 --> Could not find the language line "Contact"
+ERROR - 2016-10-21 17:50:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 383
+ERROR - 2016-10-21 17:50:28 --> Could not find the language line "Home slider"
+ERROR - 2016-10-21 17:50:34 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 17:50:34 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 17:50:36 --> Could not find the language line "Home slider"
+ERROR - 2016-10-21 17:50:39 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 17:50:39 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 17:50:40 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 17:50:40 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-21 15:50:50 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:50:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:50:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:50:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:50:59 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:03 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:08 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:23 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:42 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:51 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:51:58 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:52:00 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-21 15:52:02 --> 404 Page Not Found: Assets/plugins
