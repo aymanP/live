fd95d61c44351d18a6ca58dfd29be131d0acadd3
AymanP <elhaouari.ayman@gmail.com>
2016-10-25 11:58:58 +0200

    oh mornings

---
+++ application/logs/log-2016-10-25.php
@@ -0,0 +1,146 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-25 10:59:14 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 10:59:14 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 10:59:45 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:01:28 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 11:01:28 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 09:01:46 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:01:56 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:01:56 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:01:59 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:02:01 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:02:03 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:02:03 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:02:05 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:02:10 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:09:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:09:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:15 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:24 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:24 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:12:55 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:13:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:13:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:13:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:15:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:15:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:17:50 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:17:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:18:09 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:18:30 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:19:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:19:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 11:20:35 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 09:20:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:22:59 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:23:01 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:23:07 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:23:47 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:24:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:26:10 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:26:51 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:26:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:26:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 11:27:07 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:12 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:17 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:20 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:24 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:43 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:49 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:52 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:27:53 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 11:28:06 --> Could not find the language line "Home slider"
+ERROR - 2016-10-25 09:28:10 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:11 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:31 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:42 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:42 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:44 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 11:28:47 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 11:28:47 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-25 09:28:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:28:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:34:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:34:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:34:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:34:16 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:39:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:31 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:31 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:31 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:40:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:41:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:41:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:41:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:41:37 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:49:01 --> Severity: Compile Error --> Cannot redeclare mode_alami() (previously declared in C:\wamp64\www\CRM\live\application\helpers\perfex_misc_helper.php:574) C:\wamp64\www\CRM\live\application\helpers\perfex_db_helper.php 129
+ERROR - 2016-10-25 09:49:03 --> Severity: Compile Error --> Cannot redeclare mode_alami() (previously declared in C:\wamp64\www\CRM\live\application\helpers\perfex_misc_helper.php:574) C:\wamp64\www\CRM\live\application\helpers\perfex_db_helper.php 129
+ERROR - 2016-10-25 09:49:12 --> Severity: Compile Error --> Cannot redeclare mode_alami() (previously declared in C:\wamp64\www\CRM\live\application\helpers\perfex_misc_helper.php:574) C:\wamp64\www\CRM\live\application\helpers\perfex_db_helper.php 129
+ERROR - 2016-10-25 09:51:07 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:07 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:08 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:08 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:26 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:51:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:17 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:18 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:18 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:18 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:52:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:54:53 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:54:53 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:54:53 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:54:53 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:55:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:55:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:55:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:55:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:39 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:43 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:43 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:56:43 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:29 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:29 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:29 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:35 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:35 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:57:35 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:58:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:58:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:58:19 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-25 09:58:19 --> 404 Page Not Found: Assets/plugins
