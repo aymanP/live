e549d874cdccd88dd7c9d214286281926896b2a5
AymanP <elhaouari.ayman@gmail.com>
2016-10-26 11:33:09 +0200

    message

---
+++ application/logs/log-2016-10-26.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-26 11:01:28 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:28 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:34 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:34 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 09:01:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:01:49 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 11:01:56 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:56 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:58 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:01:58 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:02:01 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 11:02:01 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-26 09:02:10 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:25 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:40 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:41 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:48 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-26 09:02:48 --> 404 Page Not Found: Assets/plugins
