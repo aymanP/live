2a07ce3b9f8f052435f4c81707ed24addd716ecf
AymanP <elhaouari.ayman@gmail.com>
2016-10-28 11:42:59 +0200

    mm

--- application/logs/log-2016-10-28.php
+++ application/logs/log-2016-10-28.php
@@ -23,3 +23,119 @@ ERROR - 2016-10-28 11:02:19 --> Could not find the language line "Home slider"
 ERROR - 2016-10-28 11:02:27 --> Could not find the language line "Home slider"
 ERROR - 2016-10-28 11:02:33 --> Could not find the language line "Home slider"
 >>>>>>> 6dff7035d88ac2c9960faf4c699e8fff150aeb6e
+ERROR - 2016-10-28 11:06:27 --> Could not find the language line "Contact"
+ERROR - 2016-10-28 11:06:27 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:06:50 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:06:53 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:09 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:11 --> Could not find the language line "Contact"
+ERROR - 2016-10-28 11:07:11 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:18 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:18 --> Could not find the language line "Contact"
+ERROR - 2016-10-28 11:07:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\models\Projects_model.php 69
+ERROR - 2016-10-28 11:07:32 --> Could not find the language line "Contact"
+ERROR - 2016-10-28 11:07:32 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:51 --> Severity: Warning --> include_once(C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php): failed to open stream: No such file or directory C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:51 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:51 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 10
+ERROR - 2016-10-28 11:07:51 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 25
+ERROR - 2016-10-28 11:07:51 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\list_template.php 33
+ERROR - 2016-10-28 11:07:51 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> include_once(C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php): failed to open stream: No such file or directory C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 25
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\list_template.php 33
+ERROR - 2016-10-28 11:07:53 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> include_once(C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php): failed to open stream: No such file or directory C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\CRM\live\application\views/admin/etimates/estimates_top_stats.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\CRM\live\application\views\admin\projects\project_estimates.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 10
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\estimates_top_stats.php 25
+ERROR - 2016-10-28 11:07:53 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\admin\estimates\list_template.php 33
+ERROR - 2016-10-28 11:07:53 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:08:38 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:08:38 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:08:42 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:08:53 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:12 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:12 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:13 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:13 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:20 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 11:09:20 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 848
+ERROR - 2016-10-28 09:09:23 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:09:24 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:09:46 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:09:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:09:54 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:09:59 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:10:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\CRM\live\application\views\themes\perfex\template_parts\projects\project_estimates.php 27
+ERROR - 2016-10-28 09:10:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:07 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:07 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:13 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:13 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:17 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:38 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:38 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:10:50 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:10:50 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:10:50 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:14:27 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:14:27 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:14:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:14:28 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:18:29 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:18:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:18:33 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:18:33 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:18:34 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:20:03 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:20:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:20:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:20:04 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:20:32 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:20:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:20:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:20:33 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:20:33 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:21:00 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:21:00 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:21:00 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:21:00 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:21:47 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:21:47 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:21:47 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:23:57 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:23:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:23:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:23:57 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:25:21 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:25:21 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:25:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:25:22 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:26:29 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:26:30 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:26:30 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:26:30 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:30:20 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:30:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:30:20 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:30:21 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:30:21 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:33:01 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:33:02 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:33:02 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:33:02 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:33:51 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:33:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:33:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:33:52 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 11:40:55 --> Severity: Warning --> implode(): Invalid arguments passed C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 569
+ERROR - 2016-10-28 09:40:55 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:40:55 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:40:55 --> 404 Page Not Found: Assets/plugins
+ERROR - 2016-10-28 09:40:56 --> 404 Page Not Found: Assets/plugins
