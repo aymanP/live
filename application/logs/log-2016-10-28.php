6dff7035d88ac2c9960faf4c699e8fff150aeb6e
AymanP <elhaouari.ayman@gmail.com>
2016-10-28 11:03:00 +0200

    moe

---
+++ application/logs/log-2016-10-28.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-28 11:01:59 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 35 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-28 11:01:59 --> Severity: Warning --> Missing argument 1 for generate_slider(), called in C:\wamp64\www\CRM\live\application\views\authentication\includes\head.php on line 78 and defined C:\wamp64\www\CRM\live\application\helpers\perfex_html_helper.php 845
+ERROR - 2016-10-28 11:02:19 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:02:27 --> Could not find the language line "Home slider"
+ERROR - 2016-10-28 11:02:33 --> Could not find the language line "Home slider"
